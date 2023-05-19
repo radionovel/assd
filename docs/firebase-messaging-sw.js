909cf8955a8f9e1ff42580105ecd0c76c04a55fc
Андрей Радионов <radionov@gilmon.ru>
2023-05-19 11:48:10 +0500

    commit

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -1,30 +1,39 @@
-// Scripts for firebase and firebase messaging
-importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing the generated config
-const firebaseConfig = {
-    apiKey: "AIzaSyBUsCS9BkC6Er6BqTuAJqOQyGDP9MEBGi8",
-    authDomain: "gilmon-test-6c6ac.firebaseapp.com",
-    projectId: "gilmon-test-6c6ac",
-    storageBucket: "gilmon-test-6c6ac.appspot.com",
-    messagingSenderId: "104942036970",
-    appId: "1:104942036970:web:4057afdb9310988467f493",
-    measurementId: "G-59209BLT46",
-};
+firebase.initializeApp({
+    messagingSenderId: '104942036970'
+});
 
-firebase.initializeApp(firebaseConfig);
-
-// Retrieve firebase messaging
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-    console.log("Received background message ", payload);
+// Customize notification handler
+messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('Handling background message', payload);
 
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-    };
+    // Copy data object to get parameters in the click handler
+    payload.data.data = JSON.parse(JSON.stringify(payload.data));
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+    return self.registration.showNotification(payload.data.title, payload.data);
+});
+
+self.addEventListener('notificationclick', function(event) {
+    const target = event.notification.data.click_action || '/';
+    event.notification.close();
+
+    // This looks to see if the current is already open and focuses if it is
+    event.waitUntil(clients.matchAll({
+        type: 'window',
+        includeUncontrolled: true
+    }).then(function(clientList) {
+        // clientList always is empty?!
+        for (var i = 0; i < clientList.length; i++) {
+            var client = clientList[i];
+            if (client.url === target && 'focus' in client) {
+                return client.focus();
+            }
+        }
+
+        return clients.openWindow(target);
+    }));
 });
--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,135 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import {initializeApp} from "firebase/app";
+import {getMessaging, getToken, onMessage} from "firebase/messaging";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyBUsCS9BkC6Er6BqTuAJqOQyGDP9MEBGi8",
-    authDomain: "gilmon-test-6c6ac.firebaseapp.com",
-    projectId: "gilmon-test-6c6ac",
-    storageBucket: "gilmon-test-6c6ac.appspot.com",
-    messagingSenderId: "104942036970",
-    appId: "1:104942036970:web:4057afdb9310988467f493",
-    measurementId: "G-59209BLT46"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-const messaging = getMessaging(app);
-onMessage(messaging, (payload) => {
-    console.log('Message received. ', payload);
+initializeApp({
+    messagingSenderId: '448358493027'
 });
 
-function token() {
+if (
+    'Notification' in window &&
+    'serviceWorker' in navigator &&
+    'localStorage' in window &&
+    'fetch' in window &&
+    'postMessage' in window
+) {
+    var messaging = getMessaging();
 
-    getToken(messaging, {vapidKey: "BO_-C9FQb7m8HX_r8ixdRsYFBtOx75eeJt6M1WVdYQPYMKsb5sMpoOkZILnpa6-FS50HUVYnSJUaV8WxI9pRHuk"}).then((currentToken) => {
-        if (currentToken) {
-            console.log('currentToken', currentToken)
-        } else {
-            // Show permission request UI
-            console.log('No registration token available. Request permission to generate one.');
-            // ...
-        }
-    }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-        // ...
+    // already granted
+    if (Notification.permission === 'granted') {
+        getTokenFoo();
+    }
+
+    // form.on('submit', function(event) {
+    //     event.preventDefault();
+    //
+    //     var notification = {};
+    //     form.find('input').each(function () {
+    //         var input = $(this);
+    //         notification[input.attr('name')] = input.val();
+    //     });
+    //
+    //     sendNotification(notification);
+    // });
+
+    // handle catch the notification on current page
+    onMessage(messaging, function (payload) {
+        console.log('Message received', payload);
+        // register fake ServiceWorker for show notification on mobile devices
+        navigator.serviceWorker.register('/serviceworker/firebase-messaging-sw.js');
+        Notification.requestPermission(function (permission) {
+            if (permission === 'granted') {
+                navigator.serviceWorker.ready.then(function (registration) {
+                    console.log(payload)
+                    // payload.data.data = JSON.parse(JSON.stringify(payload.data));
+                    // registration.showNotification(payload.data.title, payload.data);
+                }).catch(function (error) {
+                    console.log('ServiceWorker registration failed', error);
+                });
+            }
+        });
     });
+
+    // Callback fired if Instance ID token is updated.
+    // messaging.onTokenRefresh(function() {
+    //     messaging.getToken()
+    //         .then(function(refreshedToken) {
+    //             console.log('Token refreshed');
+    //             // Send Instance ID token to app server.
+    //             sendTokenToServer(refreshedToken);
+    //             updateUIForPushEnabled(refreshedToken);
+    //         })
+    //         .catch(function(error) {
+    //             showError('Unable to retrieve refreshed token', error);
+    //         });
+    // });
+
+} else {
+    console.warn('This browser does not support desktop notification.');
+    console.log('Is HTTPS', window.location.protocol === 'https:');
+    console.log('Support Notification', 'Notification' in window);
+    console.log('Support ServiceWorker', 'serviceWorker' in navigator);
+    console.log('Support LocalStorage', 'localStorage' in window);
+    console.log('Support fetch', 'fetch' in window);
+    console.log('Support postMessage', 'postMessage' in window);
 }
 
 
-
-function requestPermission() {
-    console.log('Requesting permission...');
-    Notification.requestPermission().then((permission) => {
-        if (permission === 'granted') {
-            console.log('Notification permission granted.');
-            token()
-        }
-    });
+function getTokenFoo() {
+    // Get Instance ID token. Initially this makes a network call, once retrieved
+    // subsequent calls to getToken will return from cache.
+    const vapidKey = "BO_-C9FQb7m8HX_r8ixdRsYFBtOx75eeJt6M1WVdYQPYMKsb5sMpoOkZILnpa6-FS50HUVYnSJUaV8WxI9pRHuk";
+    getToken(messaging, {vapidKey: vapidKey})
+        .then((currentToken) => {
+            if (currentToken) {
+                sendTokenToServer(currentToken);
+            } else {
+                console.log('No Instance ID token available. Request permission to generate one');
+            }
+        })
+        .catch(function (error) {
+            console.log('An error occurred while retrieving token', error);
+        });
 }
 
-requestPermission()
+
+// function sendNotification(notification: any) {
+//     var key = 'AAAAaGQ_q2M:APA91bGCEOduj8HM6gP24w2LEnesqM2zkL_qx2PJUSBjjeGSdJhCrDoJf_WbT7wpQZrynHlESAoZ1VHX9Nro6W_tqpJ3Aw-A292SVe_4Ho7tJQCQxSezDCoJsnqXjoaouMYIwr34vZTs';
+//
+//     console.log('Send notification', notification);
+//
+//     getTokenFoo()
+//         .then(function(currentToken: string) {
+//             fetch('https://fcm.googleapis.com/fcm/send', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Authorization': 'key=' + key,
+//                     'Content-Type': 'application/json'
+//                 },
+//                 body: JSON.stringify({
+//                     data: notification,
+//                     to: currentToken
+//                 })
+//             }).then(function(response) {
+//                 return response.json();
+//             }).then(function(json) {
+//                 console.log('Response', json);
+//             }).catch(function(error) {
+//                 console.log(error);
+//             });
+//         })
+//         .catch(function(error) {
+//             console.log('Error retrieving Instance ID token', error);
+//         });
+// }
+
+// Send the Instance ID token your application server, so that it can:
+// - send messages back to this app
+// - subscribe/unsubscribe the token from topics
+function sendTokenToServer(currentToken: string | null | undefined) {
+    if (currentToken) {
+        console.log('Sending token to server...', currentToken);
+    } else {
+        console.log('Token already sent to server so won\'t send it again unless it changes');
+    }
+}
