bb96b0648db7ea35c29a34b33f98c05636d23e1f
Андрей Радионов <radionov@gilmon.ru>
2023-05-19 11:51:16 +0500

    commit

--- docs/firebase-messaging-sw.js
+++ docs/firebase-messaging-sw.js
@@ -1,7 +1,9 @@
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.7.2/firebase-messaging.js');
+// Scripts for firebase and firebase messaging
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js");
 
-firebase.initializeApp({
+// Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = {
     apiKey: "AIzaSyBUsCS9BkC6Er6BqTuAJqOQyGDP9MEBGi8",
     authDomain: "gilmon-test-6c6ac.firebaseapp.com",
     projectId: "gilmon-test-6c6ac",
@@ -9,37 +11,20 @@ firebase.initializeApp({
     messagingSenderId: "104942036970",
     appId: "1:104942036970:web:4057afdb9310988467f493",
     measurementId: "G-59209BLT46",
-});
+};
 
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
 const messaging = firebase.messaging();
 
-// Customize notification handler
-messaging.setBackgroundMessageHandler(function(payload) {
-    console.log('Handling background message', payload);
+messaging.onBackgroundMessage(function(payload) {
+    console.log("Received background message ", payload);
 
-    // Copy data object to get parameters in the click handler
-    payload.data.data = JSON.parse(JSON.stringify(payload.data));
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+    };
 
-    return self.registration.showNotification(payload.data.title, payload.data);
-});
-
-self.addEventListener('notificationclick', function(event) {
-    const target = event.notification.data.click_action || '/';
-    event.notification.close();
-
-    // This looks to see if the current is already open and focuses if it is
-    event.waitUntil(clients.matchAll({
-        type: 'window',
-        includeUncontrolled: true
-    }).then(function(clientList) {
-        // clientList always is empty?!
-        for (var i = 0; i < clientList.length; i++) {
-            var client = clientList[i];
-            if (client.url === target && 'focus' in client) {
-                return client.focus();
-            }
-        }
-
-        return clients.openWindow(target);
-    }));
+    self.registration.showNotification(notificationTitle, notificationOptions);
 });
